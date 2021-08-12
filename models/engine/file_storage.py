5a0fd3924981ed5d97007be4c3c2a00948dd4613
dontstopmeowing <dnny419@gmail.com>
2021-08-12 18:38:50 +0700

    Fix

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,13 +8,13 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
         if cls is None:
-            return self.objects
+            return FileStorage.__objects
         else:
             classes = {}
-            for k, v in self.objects.items():
+            for k, v in FileStorage.__objects.items():
                 if type(v) == cls:
                     classes[k] = v
             return classes
