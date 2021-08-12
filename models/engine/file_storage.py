7faa3578b58fd301fae065d22693aa4912167be8
dontstopmeowing <dnny419@gmail.com>
2021-08-12 18:17:10 +0700

    Fix

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -36,15 +36,24 @@ class FileStorage:
         from models.review import Review
 
         classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
+            'BaseModel': BaseModel, 'User': User, 'Place': Place,
+            'State': State, 'City': City, 'Amenity': Amenity,
+            'Review': Review
+        }
         try:
             temp = {}
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+
+def delete(self, obj=None):
+        """Deletes obj from __objects if it is inside"""
+        if obj is not None:
+            key = obj.__class__.__name__ + "." + str(obj.id)
+            if key in FileStorage.__objects:
+                FileStorage.__objects.pop(key)
+                self.save()
