ff7326aa5edd54e6bdfa948f44727e55aad8fc31
dontstopmeowing <dnny419@gmail.com>
2021-08-12 08:53:08 +0700

    Adding tests

--- tests/test_console.py
+++ tests/test_console.py
@@ -5,6 +5,7 @@ import unittest
 import console
 from unittest.mock import patch
 from io import StringIO
+import pep8
 
 hbnb_console = console.HBNBCommand
 
@@ -25,3 +26,21 @@ class ConsoleTest(unittest.TestCase):
         with patch('sys.stdout', n=StringIO()) as file:
             hbnb_console().onecmd('create State name="California"')
             file.getvalue().strip()
+
+    def test_pep8(self):
+        """Check pep8 on console"""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(["./console.py"])
+        self.assertEqual(result.total_errors, 0)
+
+    def test_docstring(self):
+        """Test for the console.py module docstring"""
+        self.assertIsNot(console.__doc__, None,
+                         "console.py does not have documentation")
+        self.assertTrue(len(console.__doc__) >= 1,
+                        "console.py does not have documentation")
+
+    def test_prompt(self):
+        """Test console prompt
+        """
+        self.assertEqual("(hbnb) ", hbnb_console.prompt)
