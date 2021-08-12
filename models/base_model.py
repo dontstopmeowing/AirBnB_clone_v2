278381db5aaee13ea274acd1b4fbd94720dd2cdf
dontstopmeowing <dnny419@gmail.com>
2021-08-12 10:45:56 +0700

    Fix

--- models/base_model.py
+++ models/base_model.py
@@ -26,11 +26,17 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
+            if "updated_at" in kwargs and "updated_at" in kwargs \
+                    and "__class__" in kwargs:
+                kwargs['updated_at'] = datetime.strptime(
+                    kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
+                kwargs['created_at'] = datetime.strptime(
+                    kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
+                del kwargs['__class__']
+            else:
+                self.id = str(uuid.uuid4())
+                self.created_at = datetime.now()
+                self.updated_at = datetime.now()
             for k, v in kwargs.items():
                 setattr(self, k, v)
             self.__dict__.update(kwargs)
