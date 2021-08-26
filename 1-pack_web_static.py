52e27aa6493ec1b32d0ba6960c86ced284fdd4e0
dontstopmeowing <dnny419@gmail.com>
2021-08-27 06:03:13 +0700

    Fix

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,13 +4,15 @@ from the contents of the web_static folder
 """
 from datetime import datetime
 from fabric.api import local
+from os.path import isdir
 from os import path
 
 
 def do_pack():
     """Generates a tgz archive."""
     date_time = datetime.now().strftime("%Y%m%d%H%M%S")
-    local("mkdir versions")
+    if isdir("versions") is False:
+        local("mkdir versions")
     archive = "versions/web_static_{}.tgz".format(date_time)
     local("tar -cvzf {} web_static".format(archive))
     if path.exists(archive):
