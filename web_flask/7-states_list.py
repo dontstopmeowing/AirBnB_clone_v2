03f605115309ce9342d3920e6ba8db0370ad11fc
dontstopmeowing <dnny419@gmail.com>
2021-09-12 01:51:56 +0700

    Adding task 8.

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""This module contains functions that starts a Flask web application.
+"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """Route /states_list"""
+    items = storage.all(State).values()
+    return render_template('7-states_list.html', states=items)
+
+
+@app.teardown_appcontext
+def storage_close(self):
+    """After each request is done, the current SQLAlchemy Session will be closed/removed."""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port=5000)
